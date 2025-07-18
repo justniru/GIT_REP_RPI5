d7e3e21a9500e206aff43565885ea5ff20d4c8ec
Niranjan Reddy <justniru@gmail.com>
2025-07-18 11:24:17 +0530

    update

---
+++ 00_Practice_PG.py
@@ -0,0 +1,44 @@
+print("Hello RPi")
+print("-----------------")
+print(9)
+print(9+9)
+print(9*9)
+print(9/3)
+print(9-3)
+print(9**2)
+print(9%2)
+print(9//2)
+print("-----------------")
+a=True
+print(a)
+print(type(a))
+b=9
+print(b)
+print(type(b))
+c=9.9
+print(c)
+print(type(c))
+d="Hello"
+print(d)
+print(type(d))
+e=[1, 2, 3]
+print(e)
+print(type(e))
+f=(1, 2, 3)
+print(f)
+print(type(f))
+g={1, 2, 3}
+print(g)
+print(type(g))
+h={"name": "John", "age": 30}
+print(h)
+print(type(h))
+i=None
+print(i)
+print(type(i))
+print("-----------------")
+def add(x, y):
+    return x + y
+j = add(5, 3)
+print(j)
+print(add(b, c))
